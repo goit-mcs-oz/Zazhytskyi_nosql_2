62bca436e8b62793f716bc1ead8d7ebbfa4e60e2
Oleksandr Zazhytskyi <alexazaz@gmai.com>
2026-07-19 18:19:00 +0300

    1.3. Отримання ембеддингів

---
+++ scripts/02_embed.py
@@ -0,0 +1,24 @@
+import os
+
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+# 1.3. Отримання ембеддингів
+
+df = pd.read_parquet("data/arxiv_subset.parquet")
+
+articles = (df['title'] + " [SEP] " + df['abstract']).tolist()
+
+model  = SentenceTransformer("allenai/specter2_base")
+embeddings = model.encode(articles, batch_size=64, show_progress_bar=True, normalize_embeddings=True)
+
+print('загальну кількість оброблених текстів', len(articles))
+print('розмірність ембеддингів', embeddings.shape)
+print('норму першого ембеддингу', np.linalg.norm(embeddings[0]))
+
+embeddings_dir = 'embeddings'
+if not os.path.isdir(embeddings_dir):
+    os.makedirs(embeddings_dir)
+
+np.save(os.path.join(embeddings_dir, 'embeddings.npy'), embeddings)
