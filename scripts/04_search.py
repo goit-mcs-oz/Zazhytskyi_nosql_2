be820a12e6d1dd03fdf6c99f2ff204483ca11d18
Oleksandr Zazhytskyi <alexazaz@gmai.com>
2026-07-23 00:32:27 +0300

    Частина 3 — Пошукові запити

--- scripts/04_search.py
+++ scripts/04_search.py
@@ -5,9 +5,10 @@ import pandas as pd
 from dotenv import load_dotenv
 from pinecone import Pinecone
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def get_query_vector(query: str, model: SentenceTransformer):
-    return model.encode(query).tolist()
+    return model.encode(query, normalize_embeddings=True).tolist()
 
 
 load_dotenv()
@@ -37,8 +38,8 @@ for item in results["matches"]:
 
 # Пошук з фільтрацією
 
-# Приклад A
 query = "reinforcement learning"
+# Приклад A
 query_vector = get_query_vector(query, model)
 results = index.query(namespace="science-papers", top_k=TOP_K, vector=query_vector, include_metadata=True,
                       filter={
@@ -59,7 +60,6 @@ for item in results["matches"]:
     print('------------------------------')
 
 # Приклад B
-query = "reinforcement learning"
 query_vector = get_query_vector(query, model)
 results = index.query(namespace="science-papers", top_k=TOP_K, vector=query_vector, include_metadata=True,
                       filter={
@@ -74,4 +74,37 @@ for item in results["matches"]:
     print('category:', item['metadata']['category'])
     print('year:', item['metadata']['year'])
     print('abstract:', item['metadata']['abstract'])
+    print('------------------------------')
+
+INPUT_EMBEDDINGS = "embeddings/embeddings.npy"
+embeddings = np.load(INPUT_EMBEDDINGS)
+
+#query = "reinforcement learning"
+
+similarity = cosine_similarity(embeddings, np.array(query_vector).reshape(1, -1)).ravel()
+top5 = np.argsort(similarity)[::-1][:TOP_K]
+print(f'\nTop-{TOP_K} статей для метрики cosine similarity:')
+for idx in top5:
+    print(f"score: {similarity[idx]:.4f}")
+    print(f"title: {df.iloc[idx]['title']}, abstract: {df.iloc[idx]['abstract']}")
+    print(f"abstract: {df.iloc[idx]['abstract']}")
+    print('------------------------------')
+
+
+dot_product = np.dot(embeddings, np.array(query_vector).reshape(1, -1).T).ravel()
+top5 = np.argsort(dot_product)[::-1][:TOP_K]
+print(f'\nTop-{TOP_K} статей для метрики dot product:')
+for idx in top5:
+    print(f"score: {similarity[idx]:.4f}")
+    print(f"title: {df.iloc[idx]['title']}, abstract: {df.iloc[idx]['abstract']}")
+    print(f"abstract: {df.iloc[idx]['abstract']}")
+    print('------------------------------')
+
+distance = np.linalg.norm(embeddings - np.array(query_vector).reshape(1, -1), axis=1)
+top5 = np.argsort(distance)[:TOP_K]
+print(f'\nTop-{TOP_K} статей для метрики L2-distance distance:')
+for idx in top5:
+    print(f"score: {distance[idx]:.4f}")
+    print(f"title: {df.iloc[idx]['title']}, abstract: {df.iloc[idx]['abstract']}")
+    print(f"abstract: {df.iloc[idx]['abstract']}")
     print('------------------------------')
